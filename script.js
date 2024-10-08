e0eb7fa2a0ca781004d821ecaaed6a6ab57a5bb2
Acciotharun1 <166792506+Acciotharun1@users.noreply.github.com>
2024-10-08 09:40:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.getElementById("my-form").addEventListener("submit",function(event){
+	event.preventDefault();
+let age=document.getElementById("age").value;
+let name=document.getElementById("name").value;
+const myPromise=new Promise((resolve,reject)=>{
+	setTimeout(()=>{
+		if(age>18){
+		resolve(name);
+		}else{
+			reject(name);
+		}
+	},4000);
+});
+myPromise.then((name)=>{
+	alert("Welcome, "+name+". You can vote.");
+}).catch((name)=>{
+	alert("Oh sorry "+name+". You aren't old enough.")
+});
+});
